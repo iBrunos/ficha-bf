b8d8925077249d6f003dbec0406717ab3135daad
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-05-24 19:56:30 -0300

    yaeh

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -29,6 +29,18 @@ function Home() {
         setCustomRace(event.target.value);
     };
 
+    const [hp, setHp] = useState(0);
+
+    const handleIncrement = () => {
+        setHp(hp + 1);
+    };
+
+    const handleDecrement = () => {
+        if (hp > 0) {
+            setHp(hp - 1);
+        }
+    };
+
     return (
         <div>
             <article className="flex-row rounded-xl border border-gray-700 bg-gray-800 p-4 w-[50rem]">
@@ -172,12 +184,41 @@ function Home() {
                                     className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
                                 />
                             </div>
+                            <div className="ml-2">
+                                <label
+                                    htmlFor="nome"
+                                    className="block text-xs font-medium text-gray-700 dark:text-gray-200"
+                                >
+                                    HP ♡
+                                </label>
+                                <div className="flex mt-1">
+                                    <button
+                                        className=" px-1 rounded-md bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white"
+                                        onClick={handleDecrement}
+                                    >
+                                        -
+                                    </button>
+                                    <input
+                                        type="number"
+                                        placeholder="0"
+                                        className="flex-1 ml-2 mr-2 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                                        value={hp}
+                                        onChange={(e) => setHp(parseInt(e.target.value))}
+                                    />
+                                    <button
+                                        className=" px-1 mr-2 rounded-md bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white"
+                                        onClick={handleIncrement}
+                                    >
+                                        +
+                                    </button>
+                                </div>
+                            </div>
                             <div className='ml-2 '>
                                 <label
                                     htmlFor="nome"
                                     className="block text-xs font-medium text-gray-700 dark:text-gray-200"
                                 >
-                                    HP
+                                    Armadura Espiritual
                                 </label>
                                 <input
                                     type="number"
@@ -185,29 +226,129 @@ function Home() {
                                     className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
                                 />
                             </div>
+                            <div className='ml-2 '>
+                                <label
+                                    htmlFor="nome"
+                                    className="block text-xs font-medium text-gray-700 dark:text-gray-200"
+                                >
+                                    Classe
+                                </label>
+                                <select
+                                    id="tendencia"
+                                    className="text-gray-950 mt-1 w-44 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                                >
+                                    <option value="">Selecione uma Classe</option>
+                                    <option value="Samurai">Samurai</option>
+                                    <option value="Ninja">Ninja</option>
+                                    <option value="Nobushi">Nobushi</option>
+                                    <option value="Shugoki">Shugoki</option>
+                                    <option value="Monge">Monge</option>
+                                    <option value="Xamã">Xamã</option>
+                                    <option value="Bruxo">Bruxo</option>
+                                    <option value="Elementalista">Elementalista</option>
+                                    <option value="Exorcista">Exorcista</option>
+                                    <option value="Artista">Artista</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                    <div className="bg-pink-900">
-                            <img
-                                alt="Developer"
-                                src={
-                                    imageSrc ||
-                                    'https://images.unsplash.com/photo-1614644147724-2d4785d69962?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
-                                }
-                                className="h-52 w-52 rounded-full object-cover px-30"
-                            />
-                            <label className='ml-10 cursor-pointer bg-gray-900 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg
-                              dark:text-white sm:text-sm' htmlFor='upload-image'>  Escolha a imagem
+                    <div className="bg-pink-900 h-68 w-52 object-cover px-30">
+                        <label
+                            htmlFor="nome"
+                            className="block text-xs font-medium text-gray-700 dark:text-gray-200"
+                        >
+                            Atributos
+                        </label>
+                        <div className='ml-2 '>
+                            <label
+                                htmlFor="nome"
+                                className="block text-xs text-gray-700 dark:text-gray-200"
+                            >
+                                Força
                             </label>
                             <input
-                                type="file"
-                                id="upload-image"
-                                accept="image/*"
-                                onChange={handleImageChange}
-                                className="hidden mt-2 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg
-                              dark:text-white sm:text-sm"
+                                type="number"
+
+                                placeholder="0"
+                                className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
                             />
 
+                        </div>
+                        <div className='ml-2 '>
+                            <label
+                                htmlFor="nome"
+                                className="block text-xs text-gray-700 dark:text-gray-200"
+                            >
+                                Inteligência
+                            </label>
+                            <input
+                                type="number"
+
+                                placeholder="0"
+                                className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                            />
+
+                        </div>
+                        <div className='ml-2 '>
+                            <label
+                                htmlFor="nome"
+                                className="block text-xs text-gray-700 dark:text-gray-200"
+                            >
+                                Espírito
+                            </label>
+                            <input
+                                type="number"
+
+                                placeholder="0"
+                                className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                            />
+
+                        </div>
+                        <div className='ml-2 '>
+                            <label
+                                htmlFor="nome"
+                                className="block text-xs text-gray-700 dark:text-gray-200"
+                            >
+                                Destreza
+                            </label>
+                            <input
+                                type="number"
+
+                                placeholder="0"
+                                className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                            />
+
+                        </div>
+                        <div className='ml-2 '>
+                            <label
+                                htmlFor="nome"
+                                className="block text-xs text-gray-700 dark:text-gray-200"
+                            >
+                                Carisma
+                            </label>
+                            <input
+                                type="number"
+
+                                placeholder="0"
+                                className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                            />
+
+                        </div>
+                        <div className='ml-2 '>
+                            <label
+                                htmlFor="nome"
+                                className="block text-xs text-gray-700 dark:text-gray-200"
+                            >
+                                Constituição
+                            </label>
+                            <input
+                                type="number"
+
+                                placeholder="0"
+                                className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                            />
+
+                        </div>
                     </div>
                 </div>
             </article>
