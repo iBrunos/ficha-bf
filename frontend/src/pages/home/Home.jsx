2dfd3045ce9db2760a9357a3c6d877c4b8c7ab91
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-05-24 18:54:51 -0300

    sim sex

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 function Home() {
     const [imageSrc, setImageSrc] = useState('');
+    const [selectedRace, setSelectedRace] = useState('');
+    const [customRace, setCustomRace] = useState('');
+
 
     function handleImageChange(event) {
         const file = event.target.files[0];
@@ -14,13 +17,24 @@ function Home() {
         reader.readAsDataURL(file);
     }
 
+    const handleRaceChange = (event) => {
+        const { value } = event.target;
+        if (value === 'outra') {
+            setCustomRace('');
+        }
+        setSelectedRace(value);
+    };
+
+    const handleCustomRaceChange = (event) => {
+        setCustomRace(event.target.value);
+    };
 
     return (
         <div>
             <article className="flex-row rounded-xl border border-gray-700 bg-gray-800 p-4 w-[50rem]">
 
                 <div className='flex flex-row'>
-                    <div className="flex-col items-center w-">
+                    <div className="bg-red-900 max-w-sm  flex-col">
                         <img
                             alt="Developer"
                             src={
@@ -31,7 +45,7 @@ function Home() {
                         />
                         <label className='ml-10 cursor-pointer bg-gray-900 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg
                           dark:text-white sm:text-sm' htmlFor='upload-image'>  Escolha a imagem
-                            
+
                         </label>
                         <input
                             type="file"
@@ -43,8 +57,8 @@ function Home() {
                         />
 
                     </div>
-                    <div className="">
-                        <ul className="-m-1 flex flex-wrap">
+                    <div className="bg-yellow-900">
+                        <ul className="ml-2 flex flex-wrap">
                             <div>
                                 <label
                                     htmlFor="nome"
@@ -55,36 +69,64 @@ function Home() {
                                 <input
                                     type="text"
                                     placeholder="Digite seu nome"
-                                    className="mt-1 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                                    className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
                                 />
                             </div>
-                            <div>
+                            <div className='ml-2 '>
                                 <label
-                                    htmlFor="raca"
+                                    htmlFor="nome"
                                     className="block text-xs font-medium text-gray-700 dark:text-gray-200"
                                 >
                                     Raça
                                 </label>
-                                <input
-                                    type="text"
-                                    placeholder="Digite seu nome"
-                                    className="mt-1 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
-                                />
+                                <select
+                                    id="raca"
+                                    className="text-gray-950 mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                                    value={selectedRace}
+                                    onChange={handleRaceChange}
+                                >
+                                    <option value="">Selecione uma raça</option>
+                                    <option value="humano">Humano comum</option>
+                                    <option value="elfo">Humano sangue puro</option>
+                                    <option value="anão">Oni</option>
+                                    <option value="orc">Kitsune</option>
+                                    <option value="halfling">Nezumi</option>
+                                    <option value="halfling">Ôkami</option>
+                                    <option value="halfling">Fukuro</option>
+                                    <option value="halfling">Elfo</option>
+                                    <option value="halfling">Tanuki</option>
+                                    <option value="halfling">Felidaes</option>
+                                    <option value="halfling">Hitsuji</option>
+                                    <option value="outra">Outra</option>
+                                </select>
+                                {selectedRace === 'outra' && (
+                                    <input
+                                        type="text"
+                                        className="ml-2 mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                                        placeholder="Digite a raça desejada"
+                                        value={customRace}
+                                        onChange={handleCustomRaceChange}
+                                    />
+                                )}
                             </div>
-                            <div>
+                        </ul>
+                        <div className='ml-2'>
                                 <label
                                     htmlFor="nome"
                                     className="block text-xs font-medium text-gray-700 dark:text-gray-200"
                                 >
                                     Idade
                                 </label>
-                                <input
-                                    type="number"
-                                    placeholder="Digite a idade"
-                                    className="mt-1 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
-                                />
+                                <div className='flex flex-row'>
+                                    <input
+                                        type="number"
+                                        placeholder="Digite a idade"
+                                        className=" mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                                    />
+
+
+                                </div>
                             </div>
-                        </ul>
                     </div>
 
 
