33430ab37421cb3b50725585fca96d7d0651a2e1
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-05-25 17:11:05 -0300

    compenente de stats pronto

--- frontend/src/pages/Atributos/atributos.jsx
+++ frontend/src/pages/Atributos/atributos.jsx
@@ -1,87 +1,144 @@
 import React, { useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
+import DoneIcon from '@mui/icons-material/Done';
 
-function Infos2() {
+function Atributos() {
+    const [heal, setHeal] = useState('');
+    const [damage, setDamage] = useState('');
+    const [Hp, setHp] = useState(0);
+    const [finalHp, setFinalHp] = useState(0);
 
-    const [selectedRace, setSelectedRace] = useState('');
-    const handleRaceChange = (event) => {
-        const { value } = event.target;
-        if (value === 'outra') {
-            setCustomRace('');
-        }
-        setSelectedRace(value);
+    const handleLeftInputChange = (e) => {
+        setHeal(e.target.value);
     };
 
-    const handleCustomRaceChange = (event) => {
-        setCustomRace(event.target.value);
+    const handleRightInputChange = (e) => {
+        setDamage(e.target.value);
     };
 
-    const [hp, setHp] = useState(0);
-
-    const handleIncrement = () => {
-        setHp(hp + 1);
+    const handleFinalHpChange = (e) => {
+        setFinalHp(parseInt(e.target.value, 10));
     };
 
-    const handleDecrement = () => {
-        if (hp > 0) {
-            setHp(hp - 1);
-        }
+    const handleDoneClick = () => {
+        const healValue = parseInt(heal) || 0;
+        const damageValue = parseInt(damage) || 0;
+        const newHp = finalHp + healValue - damageValue;
+        //const newHp = Hp + healValue - damageValue;
+        setHp(newHp);
+        setHeal('');
+        setDamage('');
+        setFinalHp(finalHp);
     };
+    const handleHpChange = (e) => {
+        setHp(parseInt(e.target.value, 10));
+      };
     return (
         <>
-            <div className='ml-2'>
-                <label
-                    htmlFor="nome"
-                    className="block text-xs font-medium text-gray-700 dark:text-gray-200"
-                >
-                    Idade
+            <div className='ml-2 mt-2 h-80 rounded-xl bg-gray-900 w-[20rem] flex flex-col'>
+                <label htmlFor='nome' className='ml-32 block font-semibold text-sm text-gray-700 dark:text-gray-200'>
+                    Stats
                 </label>
-                <div className='flex flex-row'>
+                <div className='flex flex-col'>
+                    <div className='ml-2 '>
+                        <label htmlFor='nome' className='block text-xs font-medium text-gray-700 dark:text-gray-200'>
+                            XP
+                        </label>
+                        <input
+                            type='number'
+                            placeholder='0'
+                            className='mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm'
+                        />
+                    </div>
+                </div>
+                <label htmlFor='nome' className='ml-2 block text-xs font-medium text-gray-700 dark:text-gray-200'>
+                    HP
+                </label>
+                <div>
                     <input
-                        type="number"
-                        placeholder="Digite a idade"
-                        className=" mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                        type='number'
+                        className='text-white ml-2 w-10 mt-1 rounded-md mr-2 border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm'
+                        value={Hp}
+                        disabled
+                        onChange={handleHpChange}
+                    />
+                    <span className='text-white'>/ </span>
+                    <input
+                        type='number'
+                        className='ml-2 w-10 mt-1 rounded-md mr-2 border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm'
+                        value={finalHp}
+                        onChange={handleFinalHpChange}
                     />
                 </div>
-            </div>
-            <div className='ml-2 '>
-                <label
-                    htmlFor="nome"
-                    className="block text-xs font-medium text-gray-700 dark:text-gray-200"
-                >
-                    Tendência
-                </label>
-                <select
-                    id="tendencia"
-                    className="text-gray-950 mt-1 w-44 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
-                >
-                    <option value="">Selecione uma Tendência</option>
-                    <option value="Neutro e Bom (NB)">Neutro e Bom (NB)</option>
-                    <option value="Caótico e Bom (CB)">Caótico e Bom (CB)</option>
-                    <option value="Leal e Neutro (LN)">Leal e Neutro (LN)</option>
-                    <option value="Neutro (N)">Neutro (N)</option>
-                    <option value="Caótico e Neutro (CN)">Caótico e Neutro (CN)</option>
-                    <option value="Leal e Mau (LM)">Leal e Mau (LM)</option>
-                    <option value="Neutro e Mau (NM)">Neutro e Mau (NM)</option>
-                    <option value="Caótico e Mau (CM)">Caótico e Mau (CM)</option>
-                </select>
-            </div>
-            <div className='ml-2 '>
-                <label
-                    htmlFor="nome"
-                    className="block text-xs font-medium text-gray-700 dark:text-gray-200"
-                >
-                    Nível
-                </label>
-                <input
-                    type="number"
-                    placeholder="Informe seu Nível"
-                    className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
-                />
-            </div>
 
+                <div className='flex flex-row'>
+
+                    <div className=''>
+                        <FavoriteIcon className='text-green-900 ml-2' />
+                        <input
+                            type='number'
+                            className='mt-2 ml-2 w-16 rounded-r-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm'
+                            placeholder='heal'
+                            value={heal}
+                            onChange={handleLeftInputChange}
+                        />
+                        <span className='px-1'></span>
+                        <input
+                            type='number'
+                            className='w-20 rounded-r-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm'
+                            placeholder='damage'
+                            value={damage}
+                            onChange={handleRightInputChange}
+                        />
+                        <HeartBrokenIcon className='text-red-900 mr-2 ml-2' />
+                        <button onClick={handleDoneClick}>
+                            <DoneIcon className='text-neutral-50 mr-2 ml-2' />
+                        </button>
+
+                    </div>
+                </div>
+                <div className='ml-2 '>
+                    <label
+                        htmlFor="nome"
+                        className="block text-xs font-medium text-gray-700 dark:text-gray-200"
+                    >
+                        Armadura Espiritual
+                    </label>
+                    <input
+                        type="number"
+                        placeholder=" 0"
+                        className="mt-1 w-40 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                    />
+                </div>
+                <div className='ml-2 '>
+                    <label
+                        htmlFor="nome"
+                        className="block text-xs font-medium text-gray-700 dark:text-gray-200"
+                    >
+                        Classe
+                    </label>
+                    <select
+                        id="tendencia"
+                        className="text-gray-950 mt-1 w-44 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm"
+                    >
+                        <option value="">Selecione uma Classe</option>
+                        <option value="Samurai">Samurai</option>
+                        <option value="Ninja">Ninja</option>
+                        <option value="Nobushi">Nobushi</option>
+                        <option value="Shugoki">Shugoki</option>
+                        <option value="Monge">Monge</option>
+                        <option value="Xamã">Xamã</option>
+                        <option value="Bruxo">Bruxo</option>
+                        <option value="Elementalista">Elementalista</option>
+                        <option value="Exorcista">Exorcista</option>
+                        <option value="Artista">Artista</option>
+                    </select>
+                </div>
+            </div>
         </>
 
     );
 }
 
-export default Infos2;
+export default Atributos;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
-import UploadImage from '../uploadImage/UploadImage'
-import Infos from '../infos/Infos'
-import Infos2 from '../infos2/infos2'
+import UploadImage from '../uploadImage/UploadImage';
+import Infos from '../infos/Infos';
+import Infos2 from '../infos2/infos2';
+import Atributos from '../atributos/Atributos';
 function Home() {
 
-    return (
-        <>
-          <div className='flex justify-center'>
-            <div className='rounded-xl bg-gray-900 w-[80rem] flex flex-wrap'>
-              <UploadImage></UploadImage>
-              <Infos></Infos>
-              <Infos2></Infos2>
-            </div>
-          </div>
-        </>
-      );
-      
+  return (
+
+
+
+        <Atributos></Atributos>
+
+  );
+
 }
 
+/*
+    <div className='flex justify-center'>
+      <div className='rounded-xl bg-gray-900 w-[80rem] flex flex-wrap'>
+        <UploadImage></UploadImage>
+        <div className='bg-red-900'>
+          <Atributos></Atributos>
+        </div>    
+          <div className='grid grid-cols-3'>
+            <Infos></Infos>
+            <Infos2></Infos2>
+          </div>
+        </div>
+    </div>
+
+
+
+*/
 export default Home;
