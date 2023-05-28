d4807c74c08ab9bc750b93732789b022b2187e0e
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-05-28 10:38:32 -0300

    Sabedoria ok

--- frontend/src/components/sub-attributes/SubAttributesSabedoria.jsx
+++ frontend/src/components/sub-attributes/SubAttributesSabedoria.jsx
@@ -6,38 +6,43 @@ function SubAttributesSabedoria({ modSabedoria }) {
     const [sobrevivencia, setSobrevivencia] = useState(0);
     const [perspicacia, setPerspicacia] = useState(0);
     const [proficiencia, setProficiencia] = useState(3);
-    const [toggle, setToggle] = useState(false);
+    const [toggleMedicina, setToggleMedicina] = useState(false);
+    const [toggleSobrevivencia, setToggleSobrevivencia] = useState(false);
+    const [togglePespicacia, setTgglePespicacia] = useState(false);
+    const [togglePercepcao, setTogglePercepcao] = useState(false);
 
-    const handleTogglePercepcao = () => {
-        setToggle(!toggle);
-        if (toggle) {
-            setPercepcao(percepcao - proficiencia);
-        } else {
-            setPercepcao(percepcao + proficiencia);''
-        }
-    };
+    //Logicas de Toggle de proficiencia a proficienta está sendo definifa no useState
     const handleToggleMedicina = () => {
-        setToggle(!toggle);
-        if (toggle) {
+        setToggleMedicina(!toggleMedicina);
+        if (toggleMedicina) {
             setMedicina(medicina - proficiencia);
         } else {
             setMedicina(medicina + proficiencia);
         }
     };
     const handleToggleSobrevivencia = () => {
-        setToggle(!toggle);
-        if (toggle) {
+        setToggleSobrevivencia(!toggleSobrevivencia);
+        if (toggleSobrevivencia) {
             setSobrevivencia(sobrevivencia - proficiencia);
         } else {
             setSobrevivencia(sobrevivencia + proficiencia);
         }
     };
     const handleTogglePerspicacia = () => {
-        setToggle(!toggle);
-        if (toggle) {
+        setTgglePespicacia(!togglePespicacia);
+        if (togglePespicacia) {
             setPerspicacia(perspicacia - proficiencia);
         } else {
             setPerspicacia(perspicacia + proficiencia);
+        }
+    };
+
+    const handleTogglePercepcao = () => {
+        setTogglePercepcao(!togglePercepcao);
+        if (togglePercepcao) {
+            setPercepcao(percepcao - proficiencia);
+        } else {
+            setPercepcao(percepcao + proficiencia); ''
         }
     };
 
@@ -121,23 +126,23 @@ function SubAttributesSabedoria({ modSabedoria }) {
                                 Perspicácia
                             </label>
                             <div className="flex flex-row mt-1">
-                            <input
-                                type="text"
-                                placeholder="mod"
-                                className="ml-2 mt-1 w-11 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 text-white sm:text-sm"
-                                value={perspicacia}
-                                disabled
-                            />
-                            <label className=" ml-2 relative inline-flex items-center cursor-pointer">
                                 <input
-                                    type="checkbox"
-                                    value=""
-                                    className="sr-only peer"
-                                    onClick={handleTogglePerspicacia}
+                                    type="text"
+                                    placeholder="mod"
+                                    className="ml-2 mt-1 w-11 rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 text-white sm:text-sm"
+                                    value={perspicacia}
+                                    disabled
                                 />
-                                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:mt-[0.1rem] after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                <span className="ml-3 text-sm font-medium text-white dark:text-gray-300"></span>
-                            </label>
+                                <label className=" ml-2 relative inline-flex items-center cursor-pointer">
+                                    <input
+                                        type="checkbox"
+                                        value=""
+                                        className="sr-only peer"
+                                        onClick={handleTogglePerspicacia}
+                                    />
+                                    <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:mt-[0.1rem] after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                    <span className="ml-3 text-sm font-medium text-white dark:text-gray-300"></span>
+                                </label>
                             </div>
                         </div>
                     </div>
