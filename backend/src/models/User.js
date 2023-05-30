686333e4d8e8cfd4ea94a19dc1111927c85ab29b
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-05-30 16:37:53 -0300

    sla tb

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import Ficha from './Ficha.js';
 import Toggles from './Toggles.js';
 
-
-const UserSchema = new mongoose.Schema({
+const Ficha = mongoose.model('Ficha', FichaSchema);
+ema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
