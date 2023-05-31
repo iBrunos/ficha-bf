b55d0caf8825438d8d71def198ce06844dbb4382
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-05-31 12:10:20 -0300

    Update user.service.js

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,20 +1,24 @@
 import User from '../models/User.js';
 
-const createService = (body) => User.create(body)
+const createService = (body) => User.create(body);
 
 const findAllService = () => User.find();
 
 const findByIdService = (id) => User.findById(id);
 
-const updateService = (id, username, password, level, email, phone, avatar) => User.findOneAndUpdate({_id: id},{username, password, level, email, phone, avatar});
+const updateService = (id, username, password, level, email, phone, avatar) =>
+  User.findOneAndUpdate(
+    { _id: id },
+    { username, password, level, email, phone, avatar },
+    { new: true } // Define {new: true} para retornar o documento atualizado após a atualização
+  );
 
-const deleteService = (id) => User.findOneAndDelete({_id: id});
+const deleteService = (id) => User.findOneAndDelete({ _id: id });
 
 export default {
-    createService,
-    findAllService,
-    findByIdService,
-    updateService,
-    deleteService
+  createService,
+  findAllService,
+  findByIdService,
+  updateService,
+  deleteService
 };
-
