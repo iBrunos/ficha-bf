bcd503cab1c3db2d6292bfb148d871f1abdf741f
Bruno Seixas <seixasbru@gmail.com>
2024-03-03 21:35:22 -0300

    Update main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     app.enableCors({
       origin: [
         'http://localhost:3000',
-        'https://sunx-agendamento.vercel.app',
+        'https://bladefall.vercel.app',
       ], // Lista de origens permitidas
       methods: 'GET, POST, PUT, DELETE, PATCH',
       allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
